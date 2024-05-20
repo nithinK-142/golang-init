61b998fb40cd56b27eddc85ec8d84f7adaecfd6b
nithinK-142 <nithin.sagar.359@gmail.com>
2024-05-20 16:20:18 +0530

    add: userinput.

---
+++ 03userinput/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	message := "user input"
+	fmt.Println(message)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter somethng...")
+
+	// comma ok || err
+	inp , _ := reader.ReadString('\n')
+	fmt.Println(inp)
+}
