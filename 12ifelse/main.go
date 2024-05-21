401f9f6de4e63992af7f1afe470602965b164d43
nithinK-142 <nithin.sagar.359@gmail.com>
2024-05-21 10:51:44 +0530

    add: ifelse.

---
+++ 12ifelse/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number int
+
+	fmt.Print("Enter an integer: ")
+	fmt.Scan(&number)
+
+	if number > 0 {
+		fmt.Println("The number is positive.")
+	} else if number < 0 {
+		fmt.Println("The number is negative.")
+	} else {
+		fmt.Println("The number is zero.")
+	}
+}
